d8c1f9c88cd20fb9081af884d93b884fb035ce78
Alumnos Cartagena99 <Alumnos Cartagena99@portatilB>
2019-04-16 13:36:53 +0200

    Enunciado añadido.

---
+++ BibliotecaProject/src/Enunciado/Enunciado.java
@@ -0,0 +1,31 @@
+package Enunciado;
+
+public class Enunciado {
+	/*
+	Aplicacion para biblioteca:
+
+	Dos tipos de usuarios: Administradores y bibliotecarios
+
+	Pide usuario y contraseña	//Solo los usuarios que existan podran entrar	//La contraseña no se puede guardar en tezto plano
+
+	Una aplicacion que use el administrador que de de alta y de baja al resto de usuarios y modifique el titulo de un libro
+
+	Otra aplicacion que de de alta los libros. De un libro puede haber varios ejemplares, esta app además puede:
+
+	Resto de usuarios:
+		Pedir un libro en prestamo (su maximo es de 15 dias naturales) (no puede tener mas de 3 libros)
+
+		Devolver un libro (si lo tiene)
+			(Si al devolverlo, han pasado mas de 15 dias no puede volver a cojerlo en el numero de dias*3)
+			(Si ek numero de dias supera los 200 días, el admin le elimina y si tiene otros libros se devuelven)
+		
+		Ver los libros disponibles introduciendo el titulo
+		
+		Ver todos los libros prestados o no y sus ejemplares (si estan prestados {DISPONIBLE} si no estan prestados {NO DISPONIBLE})
+		
+		Ver los libros disponibles introduciendo un autor (disponibles y no disponibles) con sus ejemplares
+
+		Modificar el numero de ejemplares que tenga un libro (cuantos me llegan o dar de baja)
+	java -jar /home/andres/test.jar
+	*/
+}
